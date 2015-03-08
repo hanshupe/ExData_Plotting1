76945667142021a11efbf82c2f935eaf181adc9f
Paul Schneeweiss <paul.schneeweiss@gmx.at>
2015-03-08 22:00:40 +0100

    Plot1 and Plot2

---
+++ plot1.R
@@ -0,0 +1,16 @@
+## load data from working directory
+data<-read.csv("household_power_consumption.txt", sep=";", na.strings = "?", stringsAsFactors=FALSE)
+
+## convert date
+datetime<-strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+data<-cbind(data, datetime)
+data$Date<-as.Date(data$Date, "%d/%m/%Y")
+
+## subquery data
+data<-subset(data, data$Date >= as.Date("01/02/2007", "%d/%m/%Y") & data$Date <= as.Date("02/02/2007", "%d/%m/%Y"))
+
+## save histogramm to disk
+opar=par(ps=12)
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col=c("red"))
+dev.copy(png,'plot1.png')
+dev.off()
